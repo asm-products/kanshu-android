a696b03f6934b093a24d5110801ed5d5b2ce3043
Victor Sima <vic.sima@gmail.com>
2015-01-30 08:27:58 -0800

    - removed duplicate com.kanshu.kanshu base package name and renamed to com.kanshu.android - added package names for activity, fragment, widget(for layouts & widgets), and adapters - renamed Model package name to model - everything builds and tests run!

---
+++ app/src/main/java/com/kanshu/android/fragment/UserAddOnsFragment.java
@@ -0,0 +1,42 @@
+package com.kanshu.android.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kanshu.android.R;
+
+/**
+ * Created by alouanemed on 26-01-2015.
+ */
+public class UserAddOnsFragment extends Fragment {
+
+    private LinearLayoutManager mLayoutManager;
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public static UserAddOnsFragment newInstance(int sectionNumber) {
+        UserAddOnsFragment fragment = new UserAddOnsFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_user_add_ons, container, false);
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        return rootView;
+    }
+
+}
