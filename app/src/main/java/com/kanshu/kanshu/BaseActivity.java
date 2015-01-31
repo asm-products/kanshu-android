f07d8e36c931fac0c4ac3272bc842856e37a9013
Michael Basil <michael@intrications.com>
2015-01-31 22:44:47 +0000

    Fix incorrect onCreate in BaseActivity

--- app/src/main/java/com/kanshu/kanshu/BaseActivity.java
+++ app/src/main/java/com/kanshu/kanshu/BaseActivity.java
@@ -2,23 +2,23 @@ package com.kanshu.kanshu;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class BaseActivity extends ActionBarActivity {
-
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
-                .setDefaultFontPath("fonts/Roboto-Regular.ttf")
-                .setFontAttrId(R.attr.fontPath)
-                .build()
+                        .setDefaultFontPath("fonts/Roboto-Regular.ttf")
+                        .setFontAttrId(R.attr.fontPath)
+                        .build()
         );
     }
+
+
 
     @Override
     protected void attachBaseContext(Context newBase) {
