cad4682b135055f4f2dcb20c5536d0215757c98c
Aran D <arangradient@gmail.com>
2015-02-01 17:07:48 -0500

    blurred text behind slider, and minor changes to dot size and thumb color

--- app/src/main/java/com/kanshu/kanshu/ReadingViewFragment.java
+++ app/src/main/java/com/kanshu/kanshu/ReadingViewFragment.java
@@ -3,16 +3,31 @@ package com.kanshu.kanshu;
 import android.animation.LayoutTransition;
 import android.app.Activity;
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.text.TextPaint;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.edmodo.rangebar.TickedSeekBar;
 
 
 /**
@@ -46,6 +61,8 @@ public class ReadingViewFragment extends Fragment {
         final LinearLayout seekBarLayout = ((LinearLayout) v.findViewById(R.id.levelSliderFrame));
         final RelativeLayout hanziLayout = ((RelativeLayout) v.findViewById(R.id.hanziSlider));
         final RelativeLayout textSizeLayout = ((RelativeLayout) v.findViewById(R.id.alphaSlider));
+        final TextView tv = ((TextView)v.findViewById(R.id.sample_text));
+        tv.setDrawingCacheEnabled(true);
         seekBarLayout.setAlpha(0.3f);
         LayoutTransition transition = new LayoutTransition();
         transition.setStagger(LayoutTransition.DISAPPEARING,1);
@@ -55,9 +72,11 @@ public class ReadingViewFragment extends Fragment {
         transition.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING,1);
         transition.setStartDelay(LayoutTransition.CHANGE_APPEARING,1);
         seekBarLayout.setLayoutTransition(transition);
+        final TickedSeekBar sb = ((TickedSeekBar)v.findViewById(R.id.levelSlider));
+        sb.setEnabled(false);
         seekBarLayout.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
+            public boolean onTouch(View v2, MotionEvent event) {
                 if(event.getAction() == MotionEvent.ACTION_DOWN){
                   if(mIsSliderCollapsed){
                       hanziLayout.setVisibility(View.VISIBLE);
@@ -73,6 +92,27 @@ public class ReadingViewFragment extends Fragment {
                               if (view == hanziLayout) {
                                   seekBarLayout.setAlpha(1.0f);
                                   seekBarLayout.getLayoutTransition().removeTransitionListener(this);
+                                  sb.setEnabled(true);
+                                  Bitmap bmp = Bitmap.createBitmap(tv.getWidth(), tv.getHeight(), Bitmap.Config.ARGB_8888);
+                                  Canvas cnv = new Canvas(bmp);
+                                  int h = seekBarLayout.getHeight();
+                                  int coordY = (int)seekBarLayout.getY();
+                                  TextPaint tPaint = tv.getPaint();
+                                  tPaint.setMaskFilter(new BlurMaskFilter(6.0f, BlurMaskFilter.Blur.NORMAL));
+                                  tPaint.setAlpha(150);
+                                 // cnv.drawText(tv.getText(),0,tv.getText().length(),0,0,tPaint);
+                                  tv.draw(cnv);
+                                  Bitmap bmpOut = Bitmap.createBitmap(seekBarLayout.getWidth(), seekBarLayout.getHeight(), Bitmap.Config.ARGB_8888);
+                                  cnv = new Canvas(bmpOut);
+                                  Paint whitePaint = new Paint();
+                                  whitePaint.setColor(getResources().getColor(R.color.background_material_light));
+                                  cnv.drawRect(0,0,seekBarLayout.getWidth(), seekBarLayout.getHeight(), whitePaint);
+                                  cnv.drawBitmap(bmp, new Rect(0, bmp.getHeight()-h, seekBarLayout.getWidth(), bmp.getHeight()), new Rect(0,0, seekBarLayout.getWidth(), seekBarLayout.getHeight()), null);
+                                  ImageView iv = (ImageView)getActivity().findViewById(R.id.blurredText);
+                                  iv.setImageBitmap(bmpOut);
+                                  tPaint.setMaskFilter(null);
+                                  tPaint.setAlpha(0);
+
                               }
                           }
                       });
@@ -93,7 +133,8 @@ public class ReadingViewFragment extends Fragment {
                         seekBarLayout.getLayoutTransition().addTransitionListener(new LayoutTransition.TransitionListener() {
                             @Override
                             public void startTransition(LayoutTransition transition, ViewGroup container, View view, int transitionType) {
-
+                                ImageView iv = (ImageView)getActivity().findViewById(R.id.blurredText);
+                                iv.setImageBitmap(null);
                             }
 
                             @Override
@@ -101,6 +142,7 @@ public class ReadingViewFragment extends Fragment {
                                 if(view == hanziLayout){
                                     seekBarLayout.setAlpha(0.3f);
                                     seekBarLayout.getLayoutTransition().removeTransitionListener(this);
+                                    sb.setEnabled(false);
                                 }
                             }
                         });
