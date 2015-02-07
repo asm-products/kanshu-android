a1c662020fdc342cbbee33414037322c6d11c3e3
Mohamed Alouane <flahweb@gmail.com>
2015-02-07 21:33:53 +0000

    Add user practice exercices screen

---
+++ app/src/main/java/com/kanshu/kanshu/Model/Exercise.java
@@ -0,0 +1,44 @@
+package com.kanshu.kanshu.model;
+
+/**
+ * Created by alouanemed on 07-02-2015.
+ */
+public class Exercise {
+    private String title;
+    private String description;
+    private String size;
+    private String thumbnail;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
