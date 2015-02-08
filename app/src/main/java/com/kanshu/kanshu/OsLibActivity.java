2a1a02d095c610724514bc004cf73c557dbc01b4
Mohamed Alouane <flahweb@gmail.com>
2015-02-08 14:28:27 +0000

    update open source lib layout

--- app/src/main/java/com/kanshu/kanshu/OsLibActivity.java
+++ app/src/main/java/com/kanshu/kanshu/OsLibActivity.java
@@ -1,40 +1,108 @@
 package com.kanshu.kanshu;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.kanshu.kanshu.model.User;
 import com.mikepenz.aboutlibraries.Libs;
 import com.mikepenz.aboutlibraries.ui.LibsFragment;
 
 
-public class OsLibActivity extends ActionBarActivity {
+public class OsLibActivity  extends BaseActivity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks,
+        NavigationDrawerFragment.NavigationDrawerData {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private SlidingTabLayout mSlidingTabLayout;
+    private OslibPagerAdapter mOslibPagerAdapter;
+    private ViewPager mViewPager;
+    private Toolbar mToolbar;
+    private User mCurrentUser;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_opensource);
+        setContentView(R.layout.activity_open_source_libs);
+        if (getIntent().hasExtra("user")) {
+            mCurrentUser = getIntent().getExtras().getParcelable("user");
+        }
+        if (mCurrentUser == null) {
+            mCurrentUser = new User("name", "level");
+        }
 
-        LibsFragment fragment = new Libs.Builder()
-                .withFields(R.string.class.getFields())
-                .withLibraries( "calligraphy", "circleimageview")
-                .withVersionShown(true)
-                .withLicenseShown(true)
-                .withLibraryModification("aboutlibraries", Libs.LibraryFields.LIBRARY_NAME, "_AboutLibraries")
-                .fragment();
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit();
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        //Set up the pager
+        mOslibPagerAdapter = new OslibPagerAdapter(
+                        getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mOslibPagerAdapter);
+        mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setSelectedIndicatorColors(getResources().getColor(R.color.white));
+        mSlidingTabLayout.setViewPager(mViewPager);
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        mToolbar.setTitle(mTitle);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_os_lib, menu);
-        return true;
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.article, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -44,11 +112,49 @@ public class OsLibActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public User getCurrentUser() {
+        return mCurrentUser;
+    }
+
+
+    public class OslibPagerAdapter extends FragmentStatePagerAdapter {
+
+        //the list of titles of pages
+        private String[] pageTitles = {"Used Open Source"};
+
+        public OslibPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            LibsFragment fragment = new Libs.Builder()
+                    .withFields(R.string.class.getFields())
+                    .withLibraries( "calligraphy", "circleimageview")
+                    .withVersionShown(true)
+                    .withLicenseShown(true)
+                    .withLibraryModification("aboutlibraries", Libs.LibraryFields.LIBRARY_NAME, "_AboutLibraries")
+                    .fragment();
+
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return pageTitles.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return pageTitles[position];
+        }
+    }
+
 }
