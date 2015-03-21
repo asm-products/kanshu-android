4e77958c497621178d7c732194315f68dbaf4dba
21zhou <21zhouyun@gmail.com>
2015-03-21 12:22:59 -0500

    added send feedback dialog

---
+++ app/src/main/java/com/kanshu/kanshu/fragment/SendFeedbackDialogFragment.java
@@ -0,0 +1,22 @@
+package com.kanshu.kanshu.fragment;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.kanshu.kanshu.R;
+
+public class SendFeedbackDialogFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState)
+    {
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        return inflater.inflate(R.layout.fragment_dialog_send_feedback, container, false);
+    }
+
+}
